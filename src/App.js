2987d85bac07c70c6627735cc2958846ee23273c
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-08-04 09:53:54 -0500

    Learning React Router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,26 @@ import { Route, Switch, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/post">
+          <NewPost />
+        </Route>
+        <Route path="/post/:id">
+          <PostPage />
+        </Route>
+        <Route path="/about" component={About} />
+        <Route path="*" component={Missing} />
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
