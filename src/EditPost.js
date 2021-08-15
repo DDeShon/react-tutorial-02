32f41797672052f2081a31c843132dcf894e177e
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-08-14 23:36:12 -0500

    Learning React Axios API Requests

--- src/EditPost.js
+++ src/EditPost.js
@@ -1,7 +1,14 @@
 import { useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 
-const EditPost = () => {
+const EditPost = ({
+  posts,
+  handleEdit,
+  editBody,
+  setEditBody,
+  editTitle,
+  setEditTitle,
+}) => {
   return <div></div>;
 };
 
