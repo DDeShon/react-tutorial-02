3e148659ea27ce098f8308a1c3ae926a11d9f7d3
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-08-14 23:44:04 -0500

    Learning React Axios API Requests

--- src/EditPost.js
+++ src/EditPost.js
@@ -18,7 +18,35 @@ const EditPost = ({
       setEditBody(post.body);
     }
   }, [post, setEditTitle, setEditBody]);
-  return <div></div>;
+  return (
+    <main className="NewPost">
+      {editTitle && (
+        <>
+          <h2>Edit Post</h2>
+          <form className="newPostForm" onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="postTitle">Title</label>
+            <input
+              id="postTitle"
+              type="text"
+              required
+              value={editTitle}
+              onChange={(e) => setEditTitle(e.target.value)}
+            />
+            <label htmlFor="postBody">Post:</label>
+            <textarea
+              id="postBody"
+              required
+              value={editBody}
+              onChange={(e) => setEditBody(e.target.value)}
+            />
+            <button type="submit" onClick={() => handleEdit(post.id)}>
+              Submit
+            </button>
+          </form>
+        </>
+      )}
+    </main>
+  );
 };
 
 export default EditPost;
