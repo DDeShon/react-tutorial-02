120c22c6135039a1b7148f799ec82d76a7790579
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-08-17 00:02:11 -0500

    Learning React Custom Hooks with Axios Async useEffect

--- src/App.js
+++ src/App.js
@@ -33,25 +33,6 @@ function App() {
     setPosts(data);
   }, [data]);
 
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     try {
-  //       const response = await api.get("/posts");
-  //       setPosts(response.data);
-  //     } catch (err) {
-  //       if (err.response) {
-  //         console.log(err.response.data);
-  //         console.log(err.response.status);
-  //         console.log(err.response.headers);
-  //       } else {
-  //         console.log(`Error: ${err.message}`);
-  //       }
-  //     }
-  //   };
-
-  //   fetchPosts();
-  // }, []);
-
   useEffect(() => {
     const filteredResults = posts.filter(
       (post) =>
