bb155e66e32654b01c59486d5a47f7eb80ae13a6
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-08-17 09:46:23 -0500

    Learning React State Management / Context API useContext

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,7 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { format } from "date-fns";
-import api from "../api/posts";
 import useAxiosFetch from "../hooks/useAxiosFetch";
 
 const DataContext = createContext({});
@@ -10,9 +7,6 @@ export const DataProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [editTitle, setEditTitle] = useState("");
-  const [editBody, setEditBody] = useState("");
-  const history = useHistory();
 
   const { data, fetchError, isLoading } = useAxiosFetch(
     "http://localhost:3500/posts"
@@ -32,22 +26,6 @@ export const DataProvider = ({ children }) => {
     setSearchResults(filteredResults.reverse());
   }, [posts, search]);
 
-  const handleEdit = async (id) => {
-    const datetime = format(new Date(), "MMMM dd, yyyy pp");
-    const updatedPost = { id, title: editTitle, datetime, body: editBody };
-    try {
-      const response = await api.put(`/posts/${id}`, updatedPost);
-      setPosts(
-        posts.map((post) => (post.id === id ? { ...response.data } : post))
-      );
-      setEditTitle("");
-      setEditBody("");
-      history.push("/");
-    } catch (err) {
-      console.log(`Error: ${err.message}`);
-    }
-  };
-
   return (
     <DataContext.Provider
       value={{
@@ -58,11 +36,6 @@ export const DataProvider = ({ children }) => {
         isLoading,
         posts,
         setPosts,
-        handleEdit,
-        editBody,
-        setEditBody,
-        editTitle,
-        setEditTitle,
       }}
     >
       {children}
